2d833dc9383da7625616f3979cdf35e1a98009ec
Cecile RUIZ <cruiz@spock07.ensta.fr>
2019-04-16 18:14:55 +0200

    la grille avec les obstacles qui s'affichent

--- Code_base/environments/EnvironnementGrille2DObs.py
+++ Code_base/environments/EnvironnementGrille2DObs.py
@@ -3,8 +3,8 @@ import random
 from environments.Environment import Environment
 import time
 
-#On numerote la grille par ordre croissant de gauche a droite puis de haut en bas 
-#Cette grille contient des obstacles
+#On numerote la grille par ordre croissant des etats de gauche a droite puis de haut en bas 
+#Cette grille contient des obstacles. Un obstacle est represente par une case noire: cet etat est inaccessible par l'agent
 
 class EnvironnementGrille2DObs(Environment):
 
@@ -41,8 +41,11 @@ class EnvironnementGrille2DObs(Environment):
         c = self.c
         l = self.l
         
-        #Liste des obstacles: un obstacle occupe un état= (ligne-1)*c+colonne
-        self.obstacle=[(int(8*l/10)-1)*c+1, (int(8*l/10)-1)*c+2, (int(8*l/10)-1)*c+3, (int(l/3)-1)*c+int(c/2), ((1+int(l/3))-1)*c+int(c/2), ((2+int(l/3))-1)*c+int(c/2),((3+int(l/3))-1)*c+int(c/2), (int(l/2)-1)*c+int(c/2), (int(l/2)-1)*c+int(c/2)+1, (int(l/2)-1)*c+int(c/2)+2]
+        #Liste des obstacles: un obstacle occupe un etat= (ligne-1)*c+colonne
+        self.obstacle=[(int(2*l/10)-1)*c+1, (int(2*l/10)-1)*c+2, (int(2*l/10)-1)*c+3, (int(l/3)-1)*c+int(c/2), (int(l/2)-1)*c+int(c/2), (int(l/2)-1)*c+int(c/2)+1, (int(l/2)-1)*c+int(c/2)+2]
+        for i in range (int(l/3), 2*int(l/3)):
+            self.obstacle += [(i-1)*c+int(c/2)]
+
         
         self.viewer = None
 
@@ -53,46 +56,54 @@ class EnvironnementGrille2DObs(Environment):
         c = self.c
         l = self.l 
         
-        #Variable qui indique si l'agent s'est cogné ou non
+        #Variable qui indique si l'agent s'est cogne ou non
         cogne=False
         
         # Decrease agent_cell by 1 if you go left, but only if you are not
         # in the left-most cell already
         # Dans la grille definie plus haut, on ne peut pas aller a gauche si le numero de la case est egal a k*colonne+1 (k entier)
-        if action==EnvironnementGrille2D.GAUCHE:
+        if action==EnvironnementGrille2DObs.GAUCHE:
             if self.current_state % c != 1 :
                 if self.current_state-1 in self.obstacle:
                     cogne=True
                 else:
                     self.current_state -= 1
+            else:
+                cogne=True                                  #L'agent s'est cogne au bord gauche
 
         # Increase agent_cell by 1 if you go right, but only if you are not
         # in the right-most cell already
         # Dans la grille definie plus haut, on ne peut pas aller a droite si le numero de la case est agal a k*colonne (k entier)
-        if action==EnvironnementGrille2D.DROITE:     
+        if action==EnvironnementGrille2DObs.DROITE:     
             if (self.current_state % c) != 0:
                 if self.current_state+1 in self.obstacle:
                     cogne=True
                 else:
                     self.current_state += 1
+            else:
+                cogne=True                                  #L'agent s'est cogne au bord droit
                 
         # Pour aller en haut dans la grille, on enleve au numero de la case le nombre colonne
         # On ne peut pas aller en haut si le numero de la case est inferieur ou egal au nombre colonne
-        if action==EnvironnementGrille2D.HAUT:     
+        if action==EnvironnementGrille2DObs.HAUT:     
             if self.current_state > c:
                 if self.current_state-c in self.obstacle:
                     cogne=True
                 else:
                     self.current_state -= c
+            else:
+                cogne=True                                  #L'agent s'est cogne en haut
         
         # Pour aller en bas dans la grille, on ajoute au numero de la case le nombre colonne
         # On ne peut pas aller en bas si le numero de la case est superieur strictement a (l-1)*c
-        if action==EnvironnementGrille2D.BAS:     
+        if action==EnvironnementGrille2DObs.BAS:     
             if self.current_state < c+(l-1):
                 if self.current_state+c in self.obstacle:
                     cogne=True
                 else:
                     self.current_state += c
+            else:
+                cogne=True                                  #L'agent s'est cogne en bas
 
         is_done = self.current_state == self.terminal_state
        
@@ -116,7 +127,7 @@ class EnvironnementGrille2DObs(Environment):
 
         # Put agent at random position (but not in a terminal state)
         cell = random.randint(1,self.num_states)
-        while cell == self.terminal_state:
+        while cell == self.terminal_state or cell in self.obstacle:
             cell = random.randint(1,self.num_states)
         self.current_state = cell
         
@@ -143,6 +154,14 @@ class EnvironnementGrille2DObs(Environment):
             self.celltrans = rendering.Transform()
             cell.add_attr(self.celltrans)
             self.viewer.add_geom(cell)
+            for o in self.obstacle:
+                obx = ((o-1)%self.c)*cell_width
+                oby = ((o-1)/self.l)*cell_height
+                ob = rendering.FilledPolygon([(l+obx,b+oby), (l+obx,t+oby), (r+obx,t+oby), (r+obx,b+oby)])
+                ob.set_color(0,0,0) # noir pour un obstacle
+                self.viewer.add_geom(ob)
+                
+                
 
         if self.current_state is None: return None
 
