e4f23b2b4f454819b7797753dd4dfa1b19004956
Cecile RUIZ <cruiz@spock07.ensta.fr>
2019-05-22 09:39:18 +0200

    Tous les environnements référencés

--- Code_base/environments/load_env.py
+++ Code_base/environments/load_env.py
@@ -3,6 +3,8 @@ __author__ = "Florence Carton"
 import sys
 from environments.EnvironmentGrid1D import EnvironmentGrid1D
 from environments.EnvironnementGrille2D import EnvironnementGrille2D
+from environments.EnvironnementGrille2DObs import EnvironnementGrille2DObs
+from environments.Labyrinthe import Labyrinthe
 
 
 
@@ -10,7 +12,9 @@ def make_env(env_name, params):
 
 	list_environments = {
 	'EnvironmentGrid1D':EnvironmentGrid1D(params),
-	'EnvironnementGrille2D':EnvironnementGrille2D(params)
+	'EnvironnementGrille2D':EnvironnementGrille2D(params),
+	'EnvironnementGrille2DObs':EnvironnementGrille2DObs(params),
+	'Labyrinthe':Labyrinthe(params)
 	}
 
 	try:
